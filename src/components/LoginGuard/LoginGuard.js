e3f5b6f7a9867b0d7875a1f062beeb67e56336f1
Đỗ Văn Thảo <thaodv4@os-MacBook-Air.local>
2022-11-26 19:29:25 +0700

    fix guard

--- src/components/LoginGuard/LoginGuard.js
+++ src/components/LoginGuard/LoginGuard.js
@@ -4,10 +4,10 @@ import { Navigate } from "react-router-dom";
 
 const LoginGuard = ({ children, ...props }) => {
   const userInfo = useSelector((state) => state.userInfoReducer.userInfo);
-  const [isLogin, setIsLogin] = useState(true);
+  const [isLogin, setIsLogin] = useState(false);
   useEffect(() => {
-    if (!userInfo) {
-      setIsLogin(false)
+    if (userInfo) {
+      setIsLogin(true)
     }
   }, [userInfo]);
   return !isLogin ? children : <Navigate to={"/"}/>;
